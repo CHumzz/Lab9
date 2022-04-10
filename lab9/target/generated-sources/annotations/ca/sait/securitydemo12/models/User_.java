4265ee6c4d6410f4d6255d6083c43a2fa941992f
Cole Humeniuk <86132515+CHumzz@users.noreply.github.com>
2022-04-10 15:58:27 -0600

    technically 100% functional
    I mean, I hard coded the beginning of the url into the link variable within AccountService and it all works now.
    
    Probably not how this was supposed to be done (and I have no idea why it wasn't doing it) but I mean she works... so...

--- lab9/target/generated-sources/annotations/ca/sait/securitydemo12/models/User_.java
+++ lab9/target/generated-sources/annotations/ca/sait/securitydemo12/models/User_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import models.Role;
 
-@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-04-10T15:02:33")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-04-10T15:55:25")
 @StaticMetamodel(User.class)
 public class User_ { 
 
