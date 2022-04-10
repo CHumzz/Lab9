b9367111e13895774a37922bb02db2cf15263562
Cole Humeniuk <86132515+CHumzz@users.noreply.github.com>
2022-04-10 13:37:09 -0600

    Binary changes - ignore, However, link still broken.
    Troubleshoooting why, when i click the link in the gmail account it breaks

--- lab9/target/generated-sources/annotations/models/Role_.java
+++ lab9/target/generated-sources/annotations/models/Role_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-04-10T13:14:44")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-04-10T13:35:51")
 @StaticMetamodel(Role.class)
 public class Role_ { 
 
