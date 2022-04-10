c2a7ceb635497767d046ec18a353e098770e670c
Cole Humeniuk <86132515+CHumzz@users.noreply.github.com>
2022-04-10 12:26:38 -0600

    idk what the f*** happened
    I get home from my travels, and Lab broken. gg. Hello World instead of login.
    
    went to Lab9 properties under "Run" and mapped /login to url. now its fine? idk man.

--- lab9/target/generated-sources/annotations/ca/sait/securitydemo12/models/User_.java
+++ lab9/target/generated-sources/annotations/ca/sait/securitydemo12/models/User_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import models.Role;
 
-@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-04-08T17:38:33")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2022-04-10T12:16:26")
 @StaticMetamodel(User.class)
 public class User_ { 
 
